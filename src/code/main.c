fc773c97c6bd7a5e77d8d735fc34d39b5e9d8a3f
Suleyman OFLAZ <suleymanoflaz1@outlook.com>
2022-07-13 16:23:46 +0300

    hook test

--- src/code/main.c
+++ src/code/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     if(argc <= 2)   printf("\nThis program calculates value of a cylinder that for given height and radius\nPlease type a height value first, and type a radius value second as arguments to program\n\n");
     else
     {
-        printf("\nCalculating the cylinder value\n\n");
+        printf("\nCalculating the cylinder value...\n\n");
         double cylinder_value=0;
 
         double height_value = atof(argv[1]);
